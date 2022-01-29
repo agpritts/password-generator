52526506c44de51bbb356d4d16919dc5288e3a8c
erma <agpritts@gmail.com>
2022-01-28 21:14:51 -0500

    intial functionality resolved

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,68 @@
 // Assignment code here
+const keys = {
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  number: "0123456789",
+  symbol: "!@#$%^&*()_+~\\`|}{[]:;?><,./-="
+}
 
+const getKey = [
+  function upperCase() {
+    return keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
+  },
+  function lowerCase() {
+    return keys.lowerCase[Math.floor(Math.random() * keys.lowerCase.length)];
+  },
+  function number() {
+    return keys.number[Math.floor(Math.random() * keys.number.length)];
+  },
+  function symbol() {
+    return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
+  }
+]
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  console.log('writePassword: ', writePassword);
+  const upper = document.getElementById("upperCase").checked;
+  const lower = document.getElementById("lowerCase").checked;
+  const number = document.getElementById("number").checked;
+  const symbol = document.getElementById("symbol").checked;
+  if (upper + lower + number + symbol === 0) {
+    alert("Please check at least one box");
+    return;
+  }
+  const passwordBox = document.getElementById("passwordBox");
+  const length = document.getElementById("length");
+  let password = "";
+  while (length.value > password.length) {
+    let keyToAdd = getKey[Math.floor(Math.random() * getKey.length)];
+    let isChecked = document.getElementById(keyToAdd.name).checked;
+    if (isChecked) {
+      password += keyToAdd();
+    }
+  }
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordBox.innerHTML = password;
 
 }
 
+// function copyPassword() {
+  // const textarea = document.createElement("textarea");
+  // const password = document.getElementById("passwordBox").innerText;
+  // if (!password) { return; }
+  // textarea.value = password;
+  // document.body.appendChild(textarea);
+  // textarea.select();
+  // document.execCommand("copy");
+  // textarea.remove();
+  // alert("Password copied to clipboard!");
+// }
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
