8e5f480105f9d7b77f929a389784d0c0506f68ee
erma <agpritts@gmail.com>
2022-01-28 21:28:29 -0500

    css fixes

--- Develop/script.js
+++ Develop/script.js
@@ -21,12 +21,8 @@ const getKey = [
   }
 ]
 
-// Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+// Write password to the #passwordBox input
 function writePassword() {
-  console.log('writePassword: ', writePassword);
   const upper = document.getElementById("upperCase").checked;
   const lower = document.getElementById("lowerCase").checked;
   const number = document.getElementById("number").checked;
@@ -45,24 +41,5 @@ function writePassword() {
       password += keyToAdd();
     }
   }
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
   passwordBox.innerHTML = password;
-
 }
-
-// function copyPassword() {
-  // const textarea = document.createElement("textarea");
-  // const password = document.getElementById("passwordBox").innerText;
-  // if (!password) { return; }
-  // textarea.value = password;
-  // document.body.appendChild(textarea);
-  // textarea.select();
-  // document.execCommand("copy");
-  // textarea.remove();
-  // alert("Password copied to clipboard!");
-// }
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
